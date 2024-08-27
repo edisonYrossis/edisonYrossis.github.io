e1c69a5bbf480b522bda18081c9b294349caf432
Edison Yadir Rossis <edisonyadir22@gmail.com>
2024-08-27 11:17:22 -0400

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const SERVER_PORT = 4000
+
+const LIVE_URL = 'https://edisonYrossis.github.io'
+
 // https://astro.build/config
 export default defineConfig({
+  server: {port: SERVER_PORT},
+  site: LIVE_URL,
   integrations: [tailwind()]
 });
