b1f094e0d3b293ebc8602c3b5f72da0c9316511a
Edison Yadir Rossis <edisonyadir22@gmail.com>
2024-09-04 17:59:58 -0400

    Adding technologies section

---
+++ src/utils/TechIconsList.js
@@ -0,0 +1,42 @@
+import JavascriptIcon from '../icons/technologies/Javascript.astro'
+import ReactIcon from '../icons/technologies/React.astro'
+import TailwindIcon from '../icons/technologies/Tailwind.astro'
+import FirebaseIcon from '../icons/technologies/Firebase.astro'
+import Node from '../icons/technologies/Node.astro'
+import Typescript from '../icons/technologies/Typescript.astro'
+
+
+export const icons = {
+    Javascript: {
+         name: "JavaScript",
+         icon: JavascriptIcon,
+         color: "#"
+     },
+    React: {
+         name: "React",
+         icon: ReactIcon,
+         color: "#"
+         
+     },
+    Tailwind: {
+         name: "Tailwind",
+         icon: TailwindIcon,
+         color: "#"
+     },
+    Firebase: {
+         name: "Firebase",
+         icon: FirebaseIcon,
+         color: "#"
+     },
+     Node: {
+        name: "Node",
+        icon: Node,
+        color: "#"
+    },
+    Typescript: {
+        name: "Typescript",
+        icon: Typescript,
+        color: "#"
+    },
+     
+ }
